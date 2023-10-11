97ebf0c55072398c27c9952ec34819a2882ccd2d
Mohammed Tahsin Kazi <k21050213@kcl.ac.uk>
2023-10-11 10:27:17 +0100

    Return median of the numbers list

--- median.py
+++ median.py
@@ -10,3 +10,10 @@ while True:
     else:
         break
 print(numbers)
+
+def findMedian(numbers):
+    numbers = sorted(numbers)
+    if len(numbers) % 2 == 1:
+        return numbers[len(numbers)//2]#It returns the middle value in the sorted list: median
+    else:
+        return (numbers[len(numbers)//2] + numbers[(len(numbers)//2)+1])/2#It returns the average of the two median values
